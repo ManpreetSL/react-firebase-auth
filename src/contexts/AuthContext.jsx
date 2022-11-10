c9ca2ede3ed0b322cb691b002662ffcb37b3dc76
Manpreet Singh Lalria <manpreet.lalria@vanti.co.uk>
2022-11-10 22:29:01 +0000

    Set up auth context. Added functionality for creating new users in Firebase

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,41 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import React, { useState, useEffect, useContext } from 'react';
+import { auth } from '../firebase';
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  function signUp(email, password) {
+    createUserWithEmailAndPassword(auth, email, password).catch((error) => {
+      console.error('Failed to sign up new user', error.code, error.message);
+    });
+  }
+
+  /**
+   * When Firebase indicates that the authorisation state has changed, updated the currently logged in user
+   */
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signUp,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export default AuthProvider;
